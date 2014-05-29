347c3fd72617d0a02a5ea4f619b3b719cbb93b1f
Bart Kerkvliet <bkerkvliet@gmail.com>
2014-05-29 16:13:30 +0200

    Added my i3-wm config
    Added my i3-wm config. It doesn't work yet, because I'm still working on my
    Conky configuration.

---
+++ i3wm/conky-i3bar-wrapper.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Send the header so that i3bar knows we want to use JSON
+echo '{"version":1}'
+
+# Open the (endless) array
+echo '['
+
+# Send an empty array first.
+echo '[],'
+
+# Now Conky will "take over" and will provide the information to display.
+exec conky -c $HOME/.config/conky/conkyjson
+
