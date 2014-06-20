f6ca10986784bde59049d5132b3127d4358ee071
Bart Kerkvliet <bkerkvliet@gmail.com>
2014-06-20 23:33:07 +0200

    WIP: Install script
    It's ugly at the moment, but it is in the very early stage. It's a work in
    progress, so probably a lot will change. I've made the script to allow easy
    installation.

---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/zsh
+
+# Directory of the script (root of repository)
+BASEDIR=$(dirname $0)
+
+# If not called from the repository root, cd to the root
+cd $BASEDIR
+
+git submodule update --init --recursive
+
+cd $BASEDIR/.zprezto
+git submodule init
+git submodule update
+
+cd $BASEDIR
+
+ln -s $BASEDIR/.i3 ~/
+ln -s $BASEDIR/.tmux ~/
+ln -s $BASEDIR/.tmux.conf ~/
+ln -s $BASEDIR/.vifm ~/
+ln -s $BASEDIR/.zprezto ~/
+ln -s $BASEDIR/.Xresources ~/
+ln -s $BASEDIR/.vimrc ~/
+ln -s $BASEDIR/git/.gitconfig ~/
+ln -s $BASEDIR/git/.gitmessage ~/
+
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+
+git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
+vim +PluginInstall +qall
