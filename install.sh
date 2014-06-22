4e754003c77e867d684939b9086b351a2a6e1016
Bart Kerkvliet <bkerkvliet@gmail.com>
2014-06-22 22:16:54 +0200

    WIP: install script huge update (NOT TESTED)
    Still work in progress. Updated my install script, but didn't properly test my
    script!

--- install.sh
+++ install.sh
@@ -6,28 +6,95 @@ BASEDIR=$(dirname $0)
 # If not called from the repository root, cd to the root
 cd $BASEDIR
 
-git submodule update --init --recursive
 
-cd $BASEDIR/.zprezto
-git submodule init
-git submodule update
+### GIT
+git=`which git 2> /dev/null`
+gitoutput=`$git --version 2> /dev/null`
+if [[ $gitoutput == *version* ]]; then
+  echo "git found"
+  echo "Getting submodules"
+  git submodule update --init --recursive
 
-cd $BASEDIR
+  echo "symlinking"
+  ln -s $BASEDIR/git/.gitconfig ~/
+  ln -s $BASEDIR/git/.gitmessage.txt ~/
+else
+  echo "git is not found, but git is required for this set-up to function."
+  exit 0
+fi
 
-ln -s $BASEDIR/.i3 ~/
-ln -s $BASEDIR/.tmux ~/
-ln -s $BASEDIR/.tmux.conf ~/
-ln -s $BASEDIR/.vifm ~/
-ln -s $BASEDIR/.zprezto ~/
-ln -s $BASEDIR/.Xresources ~/
-ln -s $BASEDIR/.vimrc ~/
-ln -s $BASEDIR/git/.gitconfig ~/
-ln -s $BASEDIR/git/.gitmessage.txt ~/
 
-setopt EXTENDED_GLOB
-for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
-done
+### I3-WM
+i3wm=`which i3 2> /dev/null`
+i3output=`$i3wm -v 2> /dev/null`
+if [[ $i3output == *version* ]]; then
+  echo "i3 found, symlinking"
+  ln -s $BASEDIR/.i3 ~/
+else
+  echo "i3 not found, skipping"
+fi
 
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
-vim +PluginInstall +qall
+
+### TMUX
+tmux=`which tmux 2> /dev/null`
+tmuxoutput=`$tmux -h 2> /dev/null`
+if [[ $tmuxoutput == *usage* ]]; then
+  echo "tmux found, symlinking"
+  ln -s $BASEDIR/.tmux ~/
+  ln -s $BASEDIR/.tmux.conf ~/
+else
+  echo "tmux not found, skipping"
+fi
+
+
+
+### VIFM
+vifm=`which vifm 2> /dev/null`
+vifmoutput=`$vifm -v 2> /dev/null`
+if [[ $vifmoutput == *Version* ]]; then
+  echo "vifm found, symlinking"
+  ln -s $BASEDIR/.vifm ~/
+else
+  echo "vifm not found, skipping"
+fi
+
+
+### VIM
+vim=`which vim 2> /dev/null`
+vimoutput=`$vim --version 2> /dev/null`
+if [[ $vimoutput == *IMproved* ]]; then
+  echo "vim found"
+  ln -s $BASEDIR/.vimrc ~/
+
+  git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
+  vim +PluginInstall +qall
+else
+  echo "vim not found, skipping"
+fi
+
+
+### XRDB
+xrdb=`which xrdb 2> /dev/null`
+xrdboutput=`$xrdb -version 2> /dev/null`
+if [[ $xrdboutput == xrdb* ]]; then
+  echo "xrdb found, symlinking Xresources"
+  ln -s $BASEDIR/.Xresources ~/
+else
+  echo "xrdb not found (Xresources)"
+fi
+
+
+### ZSH
+zsh=`which zsh 2> /dev/null`
+zshoutput=`$zsh --version 2> /dev/null`
+if [[ $zshoutput == version* ]]; then
+  echo "zsh found, symlinking"
+  ln -s $BASEDIR/.zprezto ~/
+  setopt EXTENDED_GLOB
+  for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+    ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  done
+else
+  echo "zsh not found"
+fi
+
